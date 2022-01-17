6e1bfe9e4e6d7059c8fd0e5a8c747c65b25a5f3d
Nipurnajain <nipurnabandi19@gmail.com>
2022-01-17 14:42:16 +0530

    initial commit

---
+++ build/precache-manifest.9b401208328c652ba532317f2468df67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c567667226da9b3523e9e905fc7578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c4db2a956b38651ffb",
+    "url": "/static/css/main.28f3ba4b.chunk.css"
+  },
+  {
+    "revision": "157258914840ae5d0593",
+    "url": "/static/js/2.43c6e93b.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.43c6e93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8692a46e09dbf89e03ed",
+    "url": "/static/js/3.8068ac1e.chunk.js"
+  },
+  {
+    "revision": "d9c4db2a956b38651ffb",
+    "url": "/static/js/main.e170a7e5.chunk.js"
+  },
+  {
+    "revision": "d93a2874398d36b57bb5",
+    "url": "/static/js/runtime-main.d3417531.js"
+  }
+]);
